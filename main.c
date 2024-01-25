1caf4e68c34c8d9bfc6ca71c99c6702e79435283
Roni Engel Vorst <154994175+RoniEngelVorst@users.noreply.github.com>
2024-01-25 12:00:20 +0200

    Adding the main after correcting it

--- main.c
+++ main.c
@@ -11,16 +11,6 @@ void main(){
     if(num1 <= 0 || num2 <= 0){printf("At least one of the numbers is 0 or less than 0");}
     int low = 0;
     int high = 0;
-    if(num1 < num2){ 
-        distance = num2 - num1;
-        low = num1;
-        high = num2;
-        }
-    if(num1 > num2){ 
-        distance = num1 - num2;
-        low = num2;
-        high = num1;
-        }
     if(num1 == num2){
         printf("The Armstrong numbers are:");
         if((isArmstrong(num1))){printf("%d", num1);}
@@ -30,32 +20,47 @@ void main(){
         if(isPrime(num1)){printf("%d", num1);}
         printf("\nThe Strong numbers are:");
         if(isStrong(num1)){printf("%d", num1);}
+        printf("\n");
         
     }
-    printf("The Armstrong numbers are:");
-    for(int i = 0; i<distance; i++){
-        if(isArmstrong(i+low)){
-            printf(" %d", i+low);
+    else{
+        if(num1 < num2){ 
+        distance = num2 - num1;
+        low = num1;
+        high = num2;
         }
-    }
-    printf("\nThe Palindromes are:");
-    for(int i = 0; i<distance; i++){
-        if(isPalindrome(i+low)){
-            printf(" %d", i+low);
+        if(num1 > num2){ 
+            distance = num1 - num2;
+            low = num2;
+            high = num1;
         }
-    }
-    printf("\nThe Prime numbers are:");
-    for(int i = 0; i<distance; i++){
-        if(isPrime(i+low)){
-            printf(" %d", i+low);
+    
+        printf("The Armstrong numbers are:");
+        for(int i = 0; i<distance; i++){
+            if(isArmstrong(i+low)){
+                printf(" %d", i+low);
+            }
         }
+        printf("\nThe Palindromes are:");
+        for(int i = 0; i<distance; i++){
+            if(isPalindrome(i+low)){
+                printf(" %d", i+low);
+            }
+        }
+        printf("\nThe Prime numbers are:");
+        for(int i = 0; i<distance; i++){
+            if(isPrime(i+low)){
+                printf(" %d", i+low);
+            }
+        }
+        
+        printf("\nThe Strong numbers are:");
+        for(int i = 0; i<distance; i++){
+            if(isStrong(i+low)){
+                printf(" %d", i+low);
+            }
+        }
+        printf("\n");
     }
     
-    printf("\nThe Strong numbers are:");
-    for(int i = 0; i<distance; i++){
-        if(isStrong(i+low)){
-            printf(" %d", i+low);
-        }
-    }
-    printf("\n");
 }
